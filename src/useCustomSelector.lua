7a274721b3f7fdd3d7d0a4784d575dedd49afdea
Juniper Hovey <me@juniper.link>
2023-02-28 17:53:34 -0600

    Fix selectors not properly updating selector function enclosures

--- src/useCustomSelector.lua
+++ src/useCustomSelector.lua
@@ -4,14 +4,16 @@ end
 
 local function useCustomSelector(
 	hooks,
-	selector: (state: table) -> any,
-	equalityFn: ((newState: table, oldState: table) -> boolean)?,
+	selector: (state: any) -> any,
+	equalityFn: ((newState: any, oldState: any) -> boolean)?,
 	context
 )
+	-- This value wrapper is required so the variable context of the selector function can be updated on each run --
+	local selectorFunc = hooks.useValue()
+	selectorFunc.value = selector
+
 	local store = hooks.useContext(context)
-	local mappedState, setMappedState = hooks.useState(function()
-		return selector(store:getState())
-	end)
+	local mappedState, setMappedState = hooks.useState(selector(store:getState()))
 	local oldMappedState = hooks.useValue(mappedState)
 
 	if equalityFn == nil then
@@ -20,7 +22,7 @@ local function useCustomSelector(
 
 	hooks.useEffect(function()
 		local storeChanged = store.changed:connect(function(newState, _oldState)
-			local newMappedState = selector(newState)
+			local newMappedState = selectorFunc.value(newState)
 
 			if not equalityFn(newMappedState, oldMappedState.value) then
 				oldMappedState.value = newMappedState
